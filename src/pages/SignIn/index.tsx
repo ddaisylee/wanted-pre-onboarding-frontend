d3a12ab874cad8fd4f749a16fb0042b6f57abec8
ddaisylee <elliejay9988@gmail.com>
2022-12-07 23:24:01 +0900

    feat: 로그인 페이지 UI 제작
    chore: public 폴더에 Img 폴더 및 이미지 파일 생성

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,3 +1,21 @@
-const SignIn = () => <>로그인</>
+import * as S from './styles'
+import { Button, Input } from '../../components'
+
+const SignIn = () => {
+  return (
+    <S.Container>
+      <S.LeftBox src="/Img/pre-onboarding.png" />
+      <S.RightBox>
+        <S.Title>Sign In</S.Title>
+        <S.Form>
+          <Input type="Email" />
+          <Input type="Password" />
+          <Button type="Sign In" disabled />
+          <S.Link>Sign Up</S.Link>
+        </S.Form>
+      </S.RightBox>
+    </S.Container>
+  )
+}
 
 export default SignIn
